e36d3dbe3fab240ec2e19b92dfce930add426974
Sourav Sardar <soourav8@gmail.com>
2023-03-03 18:22:58 +0530

    only 6 item show function done

--- js/index.js
+++ js/index.js
@@ -10,22 +10,29 @@ const loadTools = () => {
 //display load data
 const displayTools = tools => {
     const cards = document.getElementById('cards');
+    cards.textContent = "";
+
+    //display items only
+    tools = tools.slice(0,6);
 
     tools.forEach(tool => {
         console.log(tool);
         const cardDiv = document.createElement('div')
-        cardDiv.classList.add('card');
+        cardDiv.classList.add('col-4' );
+    
         cardDiv.innerHTML = `
 
-       
+       <div class="card h-100">
         <img src="${tool.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${tool.name}</h5>
+          <p class="card-text">${tool.description ? tool.description : 'Description not available'}</p>
         </div>
         <div class="card-footer">
           <small class="text-muted">Last updated 3 mins ago</small>
         </div>
+        </div>
+        
                 
 `
 cards.appendChild(cardDiv);
