787bb701780ca493a1a40c94d36e5d787b7d6102
Sourav Sardar <soourav8@gmail.com>
2023-03-04 12:05:25 +0530

    card footer compelete

--- js/index.js
+++ js/index.js
@@ -2,35 +2,56 @@
 const loadTools = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then(res => res.json())
-        .then(data => displayTools(data.data.tools));
+        .then(data => displayTools(data.data.tools))
+        .catch(error => console.log(error))
 
 }
 
 
+
 //display load data
+
 const displayTools = tools => {
+  console.log(tools)
+   
+    
+    
     const cards = document.getElementById('cards');
     cards.textContent = "";
 
-    //display items only
-    tools = tools.slice(0,6);
+
+
+
+
+    
 
     tools.forEach(tool => {
-        console.log(tool);
+      console.log(tool);
+        
         const cardDiv = document.createElement('div')
-        cardDiv.classList.add('col-4' );
+        cardDiv.classList.add('col-12', 'col-sm-6', 'col-md-4', );
     
-        cardDiv.innerHTML = `
+       cardDiv.innerHTML = `
 
-       <div class="card h-100">
-        <img src="${tool.image}" class="card-img-top" alt="...">
+       <div class="card h-100 rounded p-3">
+        <img  src="${tool.image}" class="card-img-top rounded " alt="...">
         <div class="card-body">
-          <h5 class="card-title">${tool.name}</h5>
-          <p class="card-text">${tool.description ? tool.description : 'Description not available'}</p>
+          <h5 class="card-title text-bold">Features</h5>
+          <h class="card-text">1. ${tool.features[0]} <br> 2. ${tool.features[1]} <br> 2. ${tool.features[2]}</p>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card-footer d-flex justify-content-between align-items-center">
+        <div>
+        <h5 class="card-title text-bold">${tool.name}</h5>
+          <small class="text-muted"><i class="fa-regular fa-calendar-days"></i> ${tool.published_in
+          }</small>
+          </div>
+          <div>
+          <div class="btn btn-light text-danger"><i class="fa-solid fa-arrow-right"></i></div>
+          </div>
         </div>
+        <div>
+        
+        <div>
         </div>
         
                 
@@ -38,6 +59,13 @@ const displayTools = tools => {
 cards.appendChild(cardDiv);
 
     });
+    
 }
 
+
+
+
+
+
+ 
 loadTools()
